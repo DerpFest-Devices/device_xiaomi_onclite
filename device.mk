d2e86851d8c9126870d95d1b857463cf77a8d0e4
ragy <wad.moskla@gmail.com>
2020-03-18 20:31:42 +0200

    onclite: 4096MB dalvik Heap configuration

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@
 
 $(call inherit-product, vendor/xiaomi/onclite/onclite-vendor.mk)
 
+# Dalvik Config
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage

01be488af719d7b212b3050d0044cc63f73de091
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-04-25 11:57:31 +0600

    onclite: Use QTI USB init configs from USB HAL repo
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Id5fd40d7e6646ab57d55dbe8846d09364738c15b

--- device.mk
+++ device.mk
@@ -316,7 +316,6 @@ PRODUCT_PACKAGES += \
     init.onclite.rc \
     init.qcom.rc \
     init.qcom.sh \
-    init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
@@ -371,6 +370,13 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 # Vibrator
 PRODUCT_PACKAGES += \

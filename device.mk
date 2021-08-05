104fa17a584d43e3e380c5667b7056e6be1d337d
bibarub <bibarub@zohomail.eu>
2021-08-05 11:59:45 +0300

    onclite: remove trust and livedisplay hals
    we don't need them now
    
    Signed-off-by: bibarub <bibarub@zohomail.eu>

--- device.mk
+++ device.mk
@@ -255,10 +255,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.onclite
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml::$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
@@ -370,10 +366,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     camera.msm8953_shim \
     libshim_dpmframework
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB HAL
 PRODUCT_PACKAGES += \
